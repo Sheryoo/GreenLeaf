78e797647f8b3aef7f218c2d161c35be24e985cc
sheryoo <sherifzayan5@gmail.com>
2024-03-29 16:26:59 +0200

    Feat: Add Endpoint for the chat bot and it's limitations to only plants

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, send_from_directory
+from controllers.chat_controller import chatBot_controller
 from models.model import create_model_table
 from models.user import create_connection, create_user_table
 from helpers.jwt_helper import auth_token
@@ -15,31 +16,44 @@ conn = create_connection()
 create_user_table(conn)
 create_model_table(conn)
 
+
 @app.route('/register', methods=['POST'])
 def register_api(): return register(app)
 
+
 @app.route('/login', methods=['POST'])
 def login_api(): return login(app)
+
 
 @app.route('/user-data', methods=['GET'])
 @auth_token
 def get_user_data_api(email): return get_user_data(email)
 
+
 @app.route('/update-user-data', methods=['PUT'])
 @auth_token
 def update_user_data_api(email): return update_user_data(email)
+
 
 @app.route('/update-password', methods=['PUT'])
 @auth_token
 def update_password_api(email): return update_password(email)
 
+
 @app.route('/uploads/<folder>/<filename>', methods=['GET'])
-def get_uploaded_file(filename,folder):
+def get_uploaded_file(filename, folder):
     return send_from_directory('public/'+folder+'/', filename)
+
 
 @app.route('/classify', methods=['POST'])
 @auth_token
 def classify_api(email): return classify_image(email)
+
+
+@app.route('/chat', methods=['POST'])
+@auth_token
+def chat_bot_api(email): return chatBot_controller()
+
 
 if __name__ == '__main__':
     app.run(host=os.getenv('HOST')
---
+++ controllers/chat_controller.py
@@ -0,0 +1,41 @@
+from flask import request, jsonify
+from dotenv import load_dotenv
+import google.generativeai as genai
+import os
+
+load_dotenv()
+
+genai.configure(api_key=os.environ["GEMINI_API_KEY"])
+model = genai.GenerativeModel('gemini-pro')
+
+
+def chatBot_controller():
+    '''Function to chat with the model'''
+    try:
+        message = request.form.get('message')
+        if not message:
+            return jsonify(
+                {
+                    "status": False,
+                    "message": "Message field is required",
+                    "data": None
+                }), 400
+        prompot_parts = [
+            'You are an AI assisstant named "GreenLeaf".You will be receiving content in various languages. Please just answer only the questions that are related to agriculture and plants or how to plant any plant else give them an answer that thier question should be related to the plants and agriclture.',
+            f"input: {message}"
+        ]
+        response = model.generate_content(prompot_parts)
+        return jsonify(
+            {
+                "status": True,
+                "message": "Model answered successfully.",
+                "data": response.text
+            }), 200
+
+    except Exception as e:
+        return jsonify(
+            {
+                "status": False,
+                "message": "Model failed to answer.",
+                "data": e
+            }), 404
