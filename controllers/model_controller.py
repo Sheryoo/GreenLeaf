38deb9535e6c3716453847ee4a7db2a3ba3012ea
sheryoo <sherifzayan5@gmail.com>
2024-06-27 15:16:23 +0300

    check if the image is a plant or not

--- controllers/model_controller.py
+++ controllers/model_controller.py
@@ -2,6 +2,7 @@ import json
 from flask import request, jsonify
 from helpers.class_names import class_names
 from helpers.generate_pdf import generate_classification_report
+from helpers.is_plant import is_plant
 from helpers.upload_images import upload_file
 from werkzeug.utils import secure_filename
 
@@ -23,6 +24,17 @@ def classify_image(email):
     plantPath = upload_file('file', 'plants')
     filename = secure_filename(file.filename)
 
+    if (not is_plant('public/plants/' + filename)):
+        return jsonify(
+            {
+                "status": False,
+                "message": "Image is not a plant",
+                "data": {
+                    'predictions': "Image is not a plant",
+                    "image": plantPath,
+                }
+            }), 200
+
     model = tf.keras.models.load_model(
         './models/my_model.h5')
     img_path = 'public/plants/' + filename
@@ -30,49 +42,34 @@ def classify_image(email):
         img_path, target_size=(128, 128))
     img_array = tf.keras.preprocessing.image.img_to_array(img)
     img_array = np.expand_dims(img_array, axis=0)
-    img_array /= 255.
 
     prediction = model.predict(img_array)
     confidence = round(100*(np.max(prediction[0])), 2)
 
-    max_probability = np.max(prediction)
+    finall_class = class_names()[np.argmax(prediction)]
+    with open("./helpers/data.json", 'r') as file:
+        plants_data = json.load(file)
 
-    if max_probability < 0.7:
-        return jsonify(
-            {
-                "status": False,
-                "message": "Image is not a plant",
-                "data": {
-                    'predictions': "Image is not a plant",
-                    "confidence": confidence,
-                    "image": plantPath,
-                }
-            }), 200
-    else:
-        finall_class = class_names()[np.argmax(prediction)]
-        with open("./helpers/data.json", 'r') as file:
-            plants_data = json.load(file)
-
-        discription = plants_data[finall_class.split(' ')[0]]["discription"]
-        temperature = plants_data[finall_class.split(' ')[0]]["temperature"]
-        sunlight = plants_data[finall_class.split(' ')[0]]["sunlight"]
-        watering = plants_data[finall_class.split(' ')[0]]["watering"]
-        add_model_data(predictions=finall_class, confidence=confidence, image=plantPath, discription=discription,
-                       temperature=temperature, sunlight=sunlight, watering=watering, userEmail=email)
-        report = generate_classification_report(
-            [finall_class, confidence, discription, temperature, sunlight, watering, email])
-        return jsonify(
-            {
-                "status": True,
-                "message": "Image classified successfully",
-                "data": {
-                    'predictions': finall_class,
-                    "confidence": confidence,
-                    "image": plantPath,
-                    "discription": discription,
-                    "temperature": temperature,
-                    "sunlight": sunlight,
-                    "watering": watering,
-                    "report_path": report
-                }
-            }), 200
+    discription = plants_data[finall_class.split(' ')[0]]["discription"]
+    temperature = plants_data[finall_class.split(' ')[0]]["temperature"]
+    sunlight = plants_data[finall_class.split(' ')[0]]["sunlight"]
+    watering = plants_data[finall_class.split(' ')[0]]["watering"]
+    add_model_data(predictions=finall_class, confidence=confidence, image=plantPath, discription=discription,
+                   temperature=temperature, sunlight=sunlight, watering=watering, userEmail=email)
+    report = generate_classification_report(
+        [finall_class, confidence, discription, temperature, sunlight, watering, email])
+    return jsonify(
+        {
+            "status": True,
+            "message": "Image classified successfully",
+            "data": {
+                'predictions': finall_class,
+                "confidence": confidence,
+                "image": plantPath,
+                "discription": discription,
+                "temperature": temperature,
+                "sunlight": sunlight,
+                "watering": watering,
+                "report_path": report
+            }
+        }), 200
